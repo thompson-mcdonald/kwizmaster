779ffb3d3e93f46d17e403e5aa305651a2488b68
thompson-mcdonald <thampsen@gmail.com>
2023-05-21 00:30:27 +0100

    feat: use Link instead of a

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react"
 import styles from "./Logo.module.css"
+import Link from "next/link"
 
 export default function Logo(props: any) {
   return (
-    <a href="/" className={styles.base}>
+    <Link href="/" className={styles.base}>
       <h1>KwizMaster</h1>
-    </a>
+    </Link>
   )
 }
